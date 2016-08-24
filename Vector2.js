c72341ceed14622d52acd8515848d89f44fe04be
Xemmas <nightshadejosh@gmail.com>
2016-08-24 22:00:23 +1000

    Collision Detection for Platformers tutorial
    An error stating that this.offset.set is not a function.

---
+++ Vector2.js
@@ -0,0 +1,43 @@
+//constructor for the vector2 object
+var Vector2 = function () {
+    this.x = 0;
+    this.y = 0;
+}
+
+//set the components of the vector object
+Vector2.prototype.Set = function (x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+//add two vectors together
+Vector2.prototype.Add = function (otherVector) {
+    var newVector = new Vector2();
+    newVector.Set(this.x + otherVector.x, this.y + otherVector.y);
+    return newVector;
+}
+
+//subtracts a vector from another vector.
+Vector2.prototype.Subtract = function (otherVector) {
+    var newVector = new Vector2();
+    newVector.Set(this.x - otherVector.x, this.y - otherVector.y);
+    return newVector;
+}
+
+//Multiplies one vector from another vector
+Vector2.prototype.Multiply = function (otherVector) {
+    var newVector = new Vector2();
+    newVector.Set(this.x * otherVector.x, this.y * otherVector.y);
+    return newVector;
+}
+
+//normalize the vector
+Vector2.prototype.Normalize = function (otherVector) {
+    var newVector = new Vector2();
+
+    if (newVector != 0) {
+        var oneOverVec = 1 / Math.sqrt(newVector);
+        this.x *= oneOverVec;
+        this.y *= oneOverVec;
+    }
+}
--- main.js
+++ main.js
@@ -40,6 +40,114 @@ var fps = 0;
 var fpsCount = 0;
 var fpsTime = 0;
 
+var cells = []; // the array that holds our simplified collision data
+function initialize() {
+    for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) { // initialize the collision map
+        cells[layerIdx] = [];
+        var idx = 0;
+        for (var y = 0; y < level1.layers[layerIdx].height; y++) {
+            cells[layerIdx][y] = [];
+            for (var x = 0; x < level1.layers[layerIdx].width; x++) {
+                if (level1.layers[layerIdx].data[idx] != 0) {
+                    // for each tile we find in the layer data, we need to create 4 collisions
+                    // (because our collision squares are 35x35 but the tile in the
+                    // level are 70x70)
+                    cells[layerIdx][y][x] = 1;
+                    cells[layerIdx][y - 1][x] = 1;
+                    cells[layerIdx][y - 1][x + 1] = 1;
+                    cells[layerIdx][y][x + 1] = 1;
+                }
+                else if (cells[layerIdx][y][x] != 1) {
+                    // if we haven't set this cell's value, then set it to 0 now
+                    cells[layerIdx][y][x] = 0;
+                }
+                idx++;
+            }
+        }
+    }
+}
+
+var MAP = { tw: 60, th: 15 };
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+var LAYER_COUNT = 3;
+var LAYER_BACKGOUND = 0;
+var LAYER_PLATFORMS = 1;
+var LAYER_LADDERS = 2;
+
+// abitrary choice for 1m
+var METER = TILE;
+// very exaggerated gravity (6x)
+var GRAVITY = METER * 9.8 * 6;
+// max horizontal speed (10 tiles per second)
+var MAXDX = METER * 10;
+// max vertical speed (15 tiles per second)
+var MAXDY = METER * 15;
+// horizontal acceleration - take 1/2 second to reach maxdx
+var ACCEL = MAXDX * 2;
+// horizontal friction - take 1/6 second to stop from maxdx
+var FRICTION = MAXDX * 6;
+// (a large) instantaneous jump impulse
+var JUMP = METER * 1500;
+
+// load the image to use for the level tiles
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+function cellAtPixelCoord(layer, x, y) {
+    if (x < 0 || x > SCREEN_WIDTH || y < 0)
+        return 1;
+    // let the player drop of the bottom of the screen (this means death)
+    if (y > SCREEN_HEIGHT)
+        return 0;
+    return cellAtTileCoord(layer, p2t(x), p2t(y));
+};
+function cellAtTileCoord(layer, tx, ty) {
+    if (tx < 0 || tx >= MAP.tw || ty < 0)
+        return 1;
+    // let the player drop of the bottom of the screen (this means death)
+    if (ty >= MAP.th)
+        return 0;
+    return cells[layer][ty][tx];
+};
+function tileToPixel(tile) {
+    return tile * TILE;
+};
+function pixelToTile(pixel) {
+    return Math.floor(pixel / TILE);
+};
+function bound(value, min, max) {
+    if (value < min)
+        return min;
+    if (value > max)
+        return max;
+    return value;
+}
+
+function drawMap() {
+    for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
+        var idx = 0;
+        for (var y = 0; y < level1.layers[layerIdx].height; y++) {
+            for (var x = 0; x < level1.layers[layerIdx].width; x++) {
+                if (level1.layers[layerIdx].data[idx] != 0) {
+                    // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+                    // correct tile
+                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_X)) * (TILESET_TILE + TILESET_SPACING);
+                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
+                }
+                idx++;
+            }
+        }
+    }
+}
+
 // load an image to draw
 var chuckNorris = document.createElement("img");
 chuckNorris.src = "hero.png";
@@ -59,7 +167,9 @@ function run()
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
 	var deltaTime = getDeltaTime();
-	
+
+	drawMap()
+
 	player.update(deltaTime);
 	player.draw();
 
@@ -82,37 +192,7 @@ function run()
 	context.fillText("FPS: " + fps, 5, 20, 100);
 }
 
-var LAYER_COUNT = 3;
-var MAP = { tw: 60, th: 15 };
-var TILE = 35;
-var TILESET_TILE = TILE * 2;
-var TILESET_PADDING = 2;
-var TILESET_SPACING = 2;
-var TILESET_COUNT_X = 14;
-var TILESET_COUNT_Y = 14;
 
-// load the image to use for the level tiles
-var tileset = document.createElement("img");
-tileset.src = "tileset.png";
-
-function drawMap() {
-    for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
-        var idx = 0;
-        for (var y = 0; y < level1.layers[layerIdx].height; y++) {
-            for (var x = 0; x < level1.layers[layerIdx].width; x++) {
-                if (level1.layers[layerIdx].data[idx] != 0) {
-                    // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
-                    // correct tile
-                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
-                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
-                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_X)) * (TILESET_TILE + TILESET_SPACING);
-                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
-                }
-                idx++;
-            }
-        }
-    }
-}
 
 //-------------------- Don't modify anything below here
 
